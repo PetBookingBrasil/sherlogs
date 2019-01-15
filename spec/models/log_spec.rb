83613b2f49b51c131d19da3fe63dd507f6a58892
lucaspesqueira <lucas@petbooking.com.br>
2019-01-15 17:44:29 -0200

    Add spec to the project

---
+++ spec/models/log_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe Log do
+  
+  describe 'validations' do
+    before do
+      log = Log.create()
+    end
+    it { }
+    it { }
+    it { }
+    it { }
+    it { }
+  end
+
+end
